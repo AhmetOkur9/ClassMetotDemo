65bff15c8c0a55154338d66fbfedc033a9f854c6
Ahmet <Ahmet@DESKTOP-H7889DL>
2022-10-06 22:47:32 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name + "  Bankanıza başarılı bir şekilde eklendi.");
+            Console.WriteLine("Hesap Bilgileri:  ");
+            Console.WriteLine("ID: "+ musteri.ID + " İsim: "+ musteri.Name +" Soyisim:  "+ musteri.Surname);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name + " " + musteri.Surname + "  Bankanızdan başarılı bir şekilde silindi. ");
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri kisi1 = new Musteri();
+            kisi1.ID = 1;
+            kisi1.Name = "Ahmet Kaan";
+            kisi1.Surname = "Okur";
+
+            Musteri kisi2 = new Musteri();
+            kisi2.ID = 2;
+            kisi2.Name = "Gökhan";
+            kisi2.Surname = "Yılmaz";
+
+            Musteri kisi3 = new Musteri();
+            kisi3.ID = 3;
+            kisi3.Name = "Dursun";
+            kisi3.Surname = "Aydın";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(kisi1);
+            Console.WriteLine("---------------------------------");
+            musteriManager.Sil(kisi2);
+            Console.WriteLine("---------------------------------");
+            musteriManager.Ekle(kisi3);
+
+
+
+
+
+        }
+
+    }
+}
